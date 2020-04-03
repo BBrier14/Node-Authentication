1cabd678c2280bc8a7a435389239a2bfa7790917
Benjamin Brier <ben.brier14@gmail.com>
2020-04-03 15:53:26 -0400

    conneected to db

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+//Connect to DB
+mongoose.connect(
+	'mongodb+srv://musashi:musashi1@cluster0-b2wid.mongodb.net/test?retryWrites=true&w=majority',
+	{ useNewUrlParser: true },
+	() => console.log('Connected to DB')
+);
 
 //Import Routes
 const authRoute = require('./routes/auth');
+
+//Route Middleware
+app.use('/api/user', authRoute);
 
 app.listen(3000, () => console.log('Server up and running'));
