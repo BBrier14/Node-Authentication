afacc43e07efb915c2ebe5e2fda56df2c1f0e931
Benjamin Brier <ben.brier14@gmail.com>
2020-04-03 15:57:30 -0400

    dotenv set up

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+dotenv.config();
+
 //Connect to DB
-mongoose.connect(
-	'mongodb+srv://musashi:musashi1@cluster0-b2wid.mongodb.net/test?retryWrites=true&w=majority',
-	{ useNewUrlParser: true },
-	() => console.log('Connected to DB')
-);
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('Connected to DB'));
 
 //Import Routes
 const authRoute = require('./routes/auth');
