9c75e8588dc851fa53dfc897ed337e9ae56da452
Benjamin Brier <ben.brier14@gmail.com>
2020-04-06 21:31:15 -0400

    catching errors, saving user

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,12 @@ router.post('/register', async (req, res) => {
 		email: req.body.email,
 		password: req.body.password
 	});
+	try {
+		const savedUser = await user.save();
+		res.send(savedUser);
+	} catch (err) {
+		res.status(400).send(err);
+	}
 });
 
 module.exports = router;
