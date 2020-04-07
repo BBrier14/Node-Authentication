c718ac2e115b7dbe3c0cb1623a942ac81950e0a3
Benjamin Brier <ben.brier14@gmail.com>
2020-04-06 21:29:34 -0400

    added middleware

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+//Import Routes
+const authRoute = require('./routes/auth');
 dotenv.config();
 
 //Connect to DB
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('Connected to DB'));
 
-//Import Routes
-const authRoute = require('./routes/auth');
+//Middleware
+app.use(express.json());
 
 //Route Middleware
 app.use('/api/user', authRoute);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
-	res.send('Register');
+router.post('/register', async (req, res) => {
+	const user = new user({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password
+	});
 });
 
 module.exports = router;
