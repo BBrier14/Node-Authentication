d3f4346c9e0e8f247bbf0d40653e84a43800ce84
Benjamin Brier <ben.brier14@gmail.com>
2020-04-06 21:39:16 -0400

    added hapi/joi package and created validation schema

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,14 @@
 const router = require('express').Router();
+const User = require('../model/User');
+
+//Validation
+const Joi = require('@hapi/joi');
+
+const schema = {
+	name: Joi.string().min(6).required(),
+	email: Joi.string().min(6).required().email(),
+	password: Joi.string().min(6).required()
+};
 
 router.post('/register', async (req, res) => {
 	const user = new user({
