b597fa01434b3d08883338ec651997aeb59da587
Benjamin Brier <ben.brier14@gmail.com>
2020-04-07 20:49:53 -0400

    checks if email is in use

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,10 @@ router.post('/register', async (req, res) => {
 	const { error } = registerValidation(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
+	//Checking if user is in database
+	const emailExist = await User.findOne({ email: req.body.email });
+	if (emailExist) return res.status(400).send('Email is already in use!');
+
 	const user = new user({
 		name: req.body.name,
 		email: req.body.email,
