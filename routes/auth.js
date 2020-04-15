fc8d8bc80bf6e54339fe9babdbdee9a7b3f6aae9
Benjamin Brier <ben.brier14@gmail.com>
2020-04-15 19:51:14 -0400

    adding tokens

--- routes/auth.js
+++ routes/auth.js
@@ -44,6 +44,9 @@ router.post('/login', async (req, res) => {
 	const validPass = await bcrypt.compare(req.body.password, user.password);
 	if (!validPass) return res.status(400).send('Invalid Password');
 
+	//Create and assign token
+	const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+
 	res.send('Logged In');
 });
 
